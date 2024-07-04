f534de6c41ced843cdc2c818867a4a5dee76d2a4
evacfk <eccel9@gmail.com>
2024-07-04 03:10:03 -0700

    Update commands_core.goundo

--- commands_core.go
+++ commands_core.go
@@ -230,7 +230,7 @@ var CoreCommands = []*commands.YAGCommand{
 				page = 1
 			}
 
-			return NewPaginatedResponse(data.GuildData.GS.ID, data.GuildData.CS.ID, page, 0, func(p *PaginatedMessage, page int) (*discordgo.MessageEmbed, error) {
+			return NewPaginatedResponse(parsed.GuildData.GS.ID, parsed.GuildData.CS.ID, page, 0, func(p *paginatedmessages.PaginatedMessage, newPage int) (*discordgo.MessageEmbed, error) {
 
 				offset := (newPage - 1) * 10
 				if offset < 0 {
