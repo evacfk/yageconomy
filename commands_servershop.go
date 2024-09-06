bd1ade14c2320b96e6881d0bba54b876acc2d3ce
evacfk <eccel9@gmail.com>
2024-09-06 13:17:41 -0700

    Update commands_servershop.go

--- commands_servershop.go
+++ commands_servershop.go
@@ -35,7 +35,8 @@ var ShopCommands = []*commands.YAGCommand{
 			account := CtxUser(parsed.Context())
 			conf := CtxConfig(parsed.Context())
 
-			_, err := paginatedmessages.CreatePaginatedMessage(parsed.GuildData.GS.ID, parsed.GuildData.CS.ID, 1, 0, func(p *paginatedmessages.PaginatedMessage, newPage int) (*discordgo.MessageEmbed, error) {
+			//_, err := paginatedmessages.CreatePaginatedMessage(parsed.GuildData.GS.ID, parsed.GuildData.CS.ID, 1, 0, func(p *paginatedmessages.PaginatedMessage, newPage int) (*discordgo.MessageEmbed, error) {
+			return paginatedmessages.NewPaginatedResponse(parsed.GuildData.GS.ID, parsed.GuildData.CS.ID, 1, 0, func(p *paginatedmessages.PaginatedMessage, newPage int) (*discordgo.MessageEmbed, error) {
 				offset := (newPage - 1) * 12
 
 				items, err := models.EconomyShopItems(
